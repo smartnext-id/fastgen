cb23393559114fe3ece3612f95be189b7a614c99
smartnext-id <tesdiabdg@gmail.com>
2025-09-15 20:14:17 +0700

    Update login.js

--- fastgen/web/js/login.js
+++ fastgen/web/js/login.js
@@ -5,7 +5,7 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
     const errorMessage = document.getElementById('error-message');
     
     try {
-        const response = await fetch('http://localhost:3001/api/login', {
+        const response = await fetch('https://fastgen-project.vercel.app/api', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ userId, password }),
